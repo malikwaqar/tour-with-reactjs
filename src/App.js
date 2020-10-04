e88901a32d099da006758a2a9a421c5306f6dd5d
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-10-04 20:41:53 +0500

    All Pages Setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home/Home';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+      <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
       </Switch>
     </Router>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../../../App.css';
-import Cards from '../../Cards/Cards';
-import Hero from '../../Hero/Hero';
+import '../../App.css';
+import Cards from '../Cards/Cards';
+import Hero from '../Hero/Hero';
 
 
 function Home() {
