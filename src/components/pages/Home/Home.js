6a457a5795f7a8662afe58ebc2c4082db8aee195
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-10-04 20:33:09 +0500

    Card and CardItem Component Completed

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../../App.css';
+import Cards from '../../Cards/Cards';
 import Hero from '../../Hero/Hero';
+
 
 function Home() {
     return (
         <>
         <Hero />
+        <Cards />
         </>
     )
 }
