8d66ed0b19d1959bfa704c424c25c66917563ff9
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-10-04 20:04:03 +0500

    Hero Component Completed

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Hero.css';
+import '../../App.css';
+import { Button } from '../Button/Button';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className="hero__buttons">
+                <Button className="btn"
+                buttonStyle="btn__outline"
+                buttonSize = "btn__large"
+                >
+                    GET STARTED
+                </Button>
+                <Button className="btn"
+                buttonStyle="btn__primary"
+                buttonSize = "btn__large"
+                >
+                    WATCH TRAILER <i className='far fa-play-circle' />
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
